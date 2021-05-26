47859ba17a416042f2582eab105f38792777ec8d
chenliangliang6 <chenliangliang6@hikvision.com.cn>
2021-05-26 16:45:10 +0800

    将项目上传到github上

---
+++ app/src/main/java/com/yimeng/aartestdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yimeng.aartestdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.concurrent.CompletableFuture;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CompletableFuture
+    }
+}
