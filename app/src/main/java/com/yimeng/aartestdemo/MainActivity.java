95f19eda679182bfb86f262d04819495be13b842
chenliangliang6 <chenliangliang6@hikvision.com.cn>
2021-05-27 14:11:10 +0800

    增加master日志的实现

--- app/src/main/java/com/yimeng/aartestdemo/MainActivity.java
+++ app/src/main/java/com/yimeng/aartestdemo/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("TAG123", "onCreate: " + "我是v1.0.2分支的内容");
+        Log.i("TAG123", "onCreate: " + "我是master分支的内容");
     }
 
 }
